3d602869ed19ebbc781c408feaad2c8f84d94b6f
neobooru <50623835+neobooru@users.noreply.github.com>
2022-08-09 00:41:19 +0200

    Fix jest puppeteer config

--- src/test/jest.d.ts
+++ src/test/jest.d.ts
@@ -5,7 +5,7 @@ declare global {
   namespace jest {
     interface Matchers<R> {
       // The `= undefined` part is incorrect according to VSCode, but it is actually required.
-      toHaveTag(tagName: string, tagCategory: TagCategory | undefined): R;
+      toHaveTag(tagName: string, tagCategory: TagCategory | undefined = undefined): R;
       toHaveNote(noteText: string): R;
       toHaveResolution(resolution: [number, number]): R;
     }
