f89ab0b949cd75a79ecc678d219f183cb867eba3
neobooru <50623835+neobooru@users.noreply.github.com>
2023-02-11 00:27:33 +0100

    Upgrade dependencies and maybe fix tests

--- src/test/jest.d.ts
+++ src/test/jest.d.ts
@@ -5,7 +5,7 @@ declare global {
   namespace jest {
     interface Matchers<R> {
       // The `= undefined` part is incorrect according to VSCode, but it is actually required.
-      toHaveTag(tagName: string, tagCategory: TagCategory | undefined = undefined): CustomMatcherResult;
+      toHaveTag(tagName: string, tagCategory?: TagCategory): CustomMatcherResult;
       toHaveNote(noteText: string): CustomMatcherResult;
       toHaveResolution(resolution: [number, number]): CustomMatcherResult;
       toHaveSource(source: string): CustomMatcherResult;
