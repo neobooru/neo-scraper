832b3d286d7319121ddb782fd748472012f3c326
Shae <jan9103.wargear@protonmail.com>
2025-04-10 11:48:53 +0200

    feat: add more domains

--- src/engines/Danbooru.ts
+++ src/engines/Danbooru.ts
@@ -6,7 +6,7 @@ export default class Danbooru extends ScrapeEngineBase {
   name = "danbooru";
   features: ScrapeEngineFeature[] = ["content", "rating", "resolution", "tags", "tag_category", "source", "notes"];
   notes = [];
-  supportedHosts = ["danbooru.donmai.us", "safebooru.donmai.us"];
+  supportedHosts = ["danbooru.donmai.us", "safebooru.donmai.us", "sonohara.donmai.us", "hijiribe.donmai.us", "hypnohub.net"];
 
   scrapeDocument(document: Document): ScrapeResult {
     const result = new ScrapeResult(this.name);
--- src/engines/Gelbooru.ts
+++ src/engines/Gelbooru.ts
@@ -13,7 +13,7 @@ export default class Gelbooru extends ScrapeEngineBase {
   name = "gelbooru";
   features: ScrapeEngineFeature[] = ["content", "rating", "resolution", "tags", "tag_category", "source", "notes"];
   notes = ["Supported features might vary between hosts."];
-  supportedHosts = ["safebooru.org", "gelbooru.com", "rule34.xxx", "tbib.org", "xbooru.com"];
+  supportedHosts = ["safebooru.org", "gelbooru.com", "rule34.xxx", "tbib.org", "xbooru.com", "mspabooru.com"];
 
   private readonly classNameToCategoryMap: CategoryMap = {
     "tag-type-copyright": "copyright",
@@ -30,6 +30,7 @@ export default class Gelbooru extends ScrapeEngineBase {
       case "rule34.xxx":
       case "tbib.org":
       case "xbooru.com":
+      case "mspabooru.com":
         version = Version.v020;
         break;
       case "gelbooru.com":
